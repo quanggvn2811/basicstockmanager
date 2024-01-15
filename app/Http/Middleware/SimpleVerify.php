7bf9b8211adb46d667e26311f41941fbab662731
quanggvn <giapvanngocquang@gmail.com>
2024-01-15 23:37:37 +0700

    Auto login if verified access token

--- app/Http/Middleware/SimpleVerify.php
+++ app/Http/Middleware/SimpleVerify.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SimpleVerify
 {
@@ -18,22 +19,41 @@ class SimpleVerify
     public function handle(Request $request, Closure $next)
     {
         // access_token = admin/staff + personalConfigToken
-        $accessToken = $request->associated_session;
+        if (!Auth::check()) {
+            $accessToken = $request->associated_session;
 
-        if (!$accessToken) {
-            abort(404);
-        }
+            if (!$accessToken) {
+                abort(404);
+            }
 
-        $role = strtolower(substr($accessToken, 0, 5));
+            $role = strtolower(substr($accessToken, 0, 5));
 
-        if (!in_array($role, self::ACCESS_ROLE)) {
-            abort(404);
-        }
+            if (!in_array($role, self::ACCESS_ROLE)) {
+                abort(404);
+            }
 
-        $canAccess = $accessToken === env('APP_ADMIN_ACCESS_TOKEN') || $accessToken === env('APP_STAFF_ACCESS_TOKEN');
+            // $canAccess = $accessToken === env('APP_ADMIN_ACCESS_TOKEN') || $accessToken === env('APP_STAFF_ACCESS_TOKEN');
 
-        if (!$canAccess) {
-            abort(404);
+            // Auto login if verified by access token
+
+            // Hard code
+            $email = 'admin@admin.com';
+            if ($role === 'staff') {
+                $email = 'staff@linhgv.com';
+            }
+
+            $password = substr($accessToken, 5);
+
+            $login = [
+                'email' => $email,
+                'password' => $password,
+            ];
+
+
+            if (!Auth::attempt($login))
+            {
+                abort(404);
+            }
         }
 
         return $next($request);
